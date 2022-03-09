a174fff2569cc5dc0521286ea820549fc628a179
Joseph Roper <1901881@uad.ac.uk>
2022-03-09 13:26:17 +0000

    Made changes to first playble

--- Assets/Scripts/TrafficCollision.cs
+++ Assets/Scripts/TrafficCollision.cs
@@ -20,11 +20,20 @@ public class TrafficCollision : MonoBehaviour
         var dist = 5.0f;
 
         RaycastHit hit1;
+        RaycastHit hit2;
         Debug.DrawRay(transform.position, Vector3.forward * dist, Color.green);
-    
-        if(Physics.Raycast(transform.position, Vector3.forward, out hit1, dist, mask))
+        Debug.DrawRay(transform.position, Vector3.left * dist, Color.green);
+
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit1, dist, mask))
         {
             
+            Debug.Log("Car hit");
+            StartCoroutine(StopCar());
+
+        }
+        if (Physics.Raycast(transform.position, Vector3.left, out hit2, dist, mask))
+        {
+
             Debug.Log("Car hit");
             StartCoroutine(StopCar());
 
