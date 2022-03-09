192daf75763b69c5aa17834a83ed81d85f36a94a
Cameron Thustain <thustain2@hotmail.co.uk>
2022-03-09 20:25:40 +0000

    finished main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -11,11 +11,5 @@ public class MainMenu : MonoBehaviour
        
     }
 
-    public void QuitGame()
-    {
-        // Application.Quit();
-        UnityEditor.EditorApplication.isPlaying = false;
-    }
-
 
 }
