2d24ed91adf6c8f9a6e1712960382950e01372f5
Joseph Roper <1901881@uad.ac.uk>
2022-04-27 18:48:51 +0100

    Added Functionality to the win state script
    need to go into each level and set the level number

--- Assets/Scripts/State Scripts/WinState.cs
+++ Assets/Scripts/State Scripts/WinState.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WinState : MonoBehaviour
 {
- 
+    public Image levelScrenshotImage;
+    public Sprite levelScreenShot1Image;
+    public Sprite levelScreenShot2Image;
+
+    private int currentLevelNumber;
+
     public void LoadMenu()
     {
         Debug.Log("Back To Menu");
@@ -17,5 +23,31 @@ public class WinState : MonoBehaviour
         Debug.Log("To Next Level");
         //load next level...
         SceneManager.LoadScene("LVL2");
+    }
+
+    public void setCurrentLevelNumber(int i) { currentLevelNumber = i; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLevelNumber = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentLevelNumber)
+        {
+            case 1:
+                levelScrenshotImage.sprite = levelScreenShot1Image;
+                break;
+            case 2:
+                levelScrenshotImage.sprite = levelScreenShot2Image;
+                break;
+            default:
+                // code block
+                levelScrenshotImage.sprite = levelScreenShot1Image;
+                break;
+        }
     }
 }
