7a739e0f051d652d749df16ab3ad78624abbefd2
Cameron Thustain <thustain2@hotmail.co.uk>
2022-05-03 14:55:07 +0100

    worked on retry button to previous level

--- Assets/Scripts/State Scripts/LoseState.cs
+++ Assets/Scripts/State Scripts/LoseState.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LoseState : MonoBehaviour
 {
-
+    public static int currentLevelNumber;
 
     public void Retry()
     {
         Debug.Log("Retry Game");
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("LevelSelect");//takes player back to level 1...
-    } 
+        //Time.timeScale = 1f;
+        //SceneManager.LoadScene("LevelSelect");//takes player back to level 1...
+        switch (currentLevelNumber)
+        {
+            case 1:
+                //if current level one load level 1 on retry
+                Time.timeScale = 1f;
+                SceneManager.LoadScene("LVL1");
+                break;
+            case 2:
+                //if current level two load level 2 on retry
+                Time.timeScale = 1f;
+                SceneManager.LoadScene("LVL2");
+                break;
+            case 3:
+                //if current level three load level 3 on retry
+                Time.timeScale = 1f;
+                SceneManager.LoadScene("LVL3");
+                break;
+            default:
+                Debug.Log("not working");
+                break;
+        }
+    }
 
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");//goes back to main menu
 
     }
+
+    
 }
