2411c4221b1d870b75d994c57b1f13eeefa3e0ef
1901881 <59955717+1901881@users.noreply.github.com>
2022-05-04 15:06:51 +0100

    Added Alis numbers to the counter (#107)
    Every number image has a different size so when it is switched the sizing goes off

--- Assets/Scripts/State Scripts/AntEaterCounter.cs
+++ Assets/Scripts/State Scripts/AntEaterCounter.cs
@@ -8,6 +8,7 @@ public class AntEaterCounter : MonoBehaviour
     public int maxCounter;
 
     public Image counterImage;
+    public Sprite counter0Image;
     public Sprite counter1Image;
     public Sprite counter2Image;
     public Sprite counter3Image;
@@ -20,6 +21,9 @@ public class AntEaterCounter : MonoBehaviour
     void Start()
     {
         currentCounter = maxCounter;
+
+
+     
     }
 
     // Update is called once per frame
@@ -28,28 +32,36 @@ public class AntEaterCounter : MonoBehaviour
         switch (currentCounter)
         {
             case 0:
-                counterImage.sprite = counter1Image;
+                counterImage.sprite = counter0Image;
+                counterImage.rectTransform.sizeDelta = new Vector2(30, 30);
                 break;
             case 1:
                 counterImage.sprite = counter1Image;
+                counterImage.rectTransform.sizeDelta = new Vector2(30, 30);
                 break;
             case 2:
                 counterImage.sprite = counter2Image;
+                counterImage.rectTransform.sizeDelta = new Vector2(30, 30);
                 break;
             case 3:
                 counterImage.sprite = counter3Image;
+                counterImage.rectTransform.sizeDelta = new Vector2(30, 30);
                 break;
             case 4:
                 counterImage.sprite = counter4Image;
+                counterImage.rectTransform.sizeDelta = new Vector2(30, 30);
                 break;
             case 5:
                 counterImage.sprite = counter5Image;
+                counterImage.rectTransform.sizeDelta = new Vector2(30, 30);
                 break;
             default:
                 // code block
-                counterImage.sprite = counter1Image;
+                counterImage.sprite = counter0Image;
+                counterImage.rectTransform.sizeDelta = new Vector2(30, 30);
                 break;
         }
+      
     }
 
     public void setCounterDown() { currentCounter -= 1; }
